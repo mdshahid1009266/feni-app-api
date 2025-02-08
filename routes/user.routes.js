7502d2fb93e73917095c5a2fa582d1a2ea02fc95
mdshahid1009266 <148151453+mdshahid1009266@users.noreply.github.com>
2025-02-09 05:17:51 +0600

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 // import { authenticateToken } from '../middlewares/auth.middleware.js';
 import { signup, login, getAllUsers } from '../controllers/user.controllers.js';
 
-router.get('/getAllUsers', getAllUsers);
+//router.get('/getAllUsers', getAllUsers);
 router.post('/signup', signup);
 router.post('/login', login);
 
